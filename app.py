2574f84fae32e9e86a7a3781760c2aa8a542b1e2
su41 <su41@cms-r1-16.cms.waikato.ac.nz>
2019-06-07 14:41:22 +1200

    Updated the prime number method because I entered 1 digit numbers and errors were thrown around. This new method handles all prime numbers well. Also implemented a clear list method just for testing purposes

--- app.py
+++ app.py
@@ -28,23 +28,40 @@ def hello():
 
 @app.route('/isPrime/<int:number>')
 def isPrimeNumber(number):
-#need to make sure that the datatype parameters are also within the def so that it's defined. 
-	#check if the number entered is a prime 
-	if number > 1:
-		for i in range(2, number//2):
-			if(number % i) ==0:
-				return '{} is not a prime number.\n'.format(number)
-				break
-			else:
-				cache.lpush('primeList', number)
-				return '{} is a prime number.\n'.format(number)
-
-	else:
+	if (number <=1): #get rid of 1s, 0s and anything less than that since they are not prime 
 		return 'Please enter a number greater than 1.\n'
 
+	if (number <=3): #catch entries of 2 and 3 before we start checking remainders as 2 and 3 are prime but 2%2 = 0 
+		cache.lpush('primeList', number)
+		return '{} is a prime number.\n'.format(number)
+		
+	if(number % 2 == 0 or number % 3 == 0): #if the number isn't <=3, but can be % by 2 or 3 and it equals zero, it's not prime 
+		return '{} is not a prime number.\n'.format(number)
 
+	i = 5
+	while(i * i <number):
+		if (number % i == 0 or number %(i + 2) ==0): 
+			return '{} is not a prime number.\n'.format(number)
+		#we start from 5 and go up by 6 each iteration, 
+		
+		i = i + 6
+	cache.lpush('primeList', number)
+	return '{} is a prime number.\n'.format(number)
+
+			
+	
+	
 @app.route('/primesStored')
 def listPrime():	
 	#lrange might be the easiest way to return list of everything that goes into primeList 
 	return 'length is {}.\n'.format(cache.lrange('primeList', 0, -1)) #this works 
+
+
+
+#this clear list method is to obviously clear the list, and reset it for testing purposes 
+@app.route('/clearList')
+def listClear():
+	for i in range (0, cache.llen('primeList')):
+		cache.lpop('primeList') 
+	return 'List has been cleared.\n'
 	
